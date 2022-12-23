f3750c155cac817c8ab9421824d1b2bf4a9e2c6c
SirYwell <hannesgreule@outlook.de>
2022-12-23 19:36:28 +0100

    Use better cache invalidation strategy

--- src/main/kotlin/de/sirywell/methodhandleplugin/dfa/MhTypeProvider.kt
+++ src/main/kotlin/de/sirywell/methodhandleplugin/dfa/MhTypeProvider.kt
@@ -6,11 +6,12 @@ import com.intellij.psi.PsiFile
 import com.intellij.psi.util.CachedValueProvider
 import com.intellij.psi.util.ParameterizedCachedValue
 import com.intellij.psi.util.ParameterizedCachedValueProvider
+import com.intellij.psi.util.PsiModificationTracker
 import de.sirywell.methodhandleplugin.TypeData
 
 object MhTypeProvider : ParameterizedCachedValueProvider<TypeData, PsiFile> {
     override fun compute(param: PsiFile): CachedValueProvider.Result<TypeData> {
-        return CachedValueProvider.Result.create(MethodHandleElementVisitor().scan(param), ModificationTracker.EVER_CHANGED)
+        return CachedValueProvider.Result.create(MethodHandleElementVisitor().scan(param), PsiModificationTracker.MODIFICATION_COUNT)
     }
 
     val CACHE_KEY = Key<ParameterizedCachedValue<TypeData, PsiFile>>("HandleHints.Cache")
