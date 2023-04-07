e33ad51ea62c2f5c98ba30bf4c1554bf0add17d7
Hannes Greule <SirYwell@users.noreply.github.com>
2023-04-07 09:03:25 +0200

    feat: Support methods from MethodType (#13)

--- src/main/kotlin/de/sirywell/methodhandleplugin/dfa/SsaAnalyzer.kt
+++ src/main/kotlin/de/sirywell/methodhandleplugin/dfa/SsaAnalyzer.kt
@@ -80,9 +80,65 @@ class SsaAnalyzer(private val controlFlow: ControlFlow, private val typeData: Ty
     private fun resolveMhType(expression: PsiExpression, block: Block): MhType? {
         if (expression is PsiMethodCallExpression) {
             val arguments = expression.argumentList.expressions.asList()
+            val methodExpression = expression.methodExpression
+            val qualifier = methodExpression.qualifierExpression
             if (receiverIsMethodType(expression)) {
                 return when (expression.methodName) {
-                    "methodType" -> MethodTypeHelper.methodType(arguments.toPsiTypes() ?: return notConstant())
+                    "methodType" -> {
+                        if (arguments.isEmpty()) return noMatch()
+                        if (arguments.size == 2 && arguments[1].type == methodTypeType(expression)) {
+                            MethodTypeHelper.methodType(arguments[0], arguments[1].mhType(block) ?: return notConstant())
+                        } else {
+                            MethodTypeHelper.methodType(arguments)
+                        }
+                    }
+                    "unwrap" -> MethodTypeHelper.unwrap(qualifier?.mhType(block) ?: return noMatch())
+                    "wrap" -> MethodTypeHelper.wrap(expression, qualifier?.mhType(block) ?: return noMatch())
+                    "dropParameterTypes" -> {
+                        val mhType = qualifier?.mhType(block) ?: return noMatch()
+                        if (arguments.size != 2) return noMatch()
+                        val (start, end) = arguments
+                        MethodTypeHelper.dropParameterTypes(mhType, start, end)
+                    }
+
+                    "insertParameterTypes" -> {
+                        val mhType = qualifier?.mhType(block) ?: return noMatch()
+                        if (arguments.isEmpty()) return noMatch()
+                        MethodTypeHelper.insertParameterTypes(mhType, arguments[0], arguments.drop(1))
+                    }
+
+                    "changeParameterType" -> {
+                        val mhType = qualifier?.mhType(block) ?: return noMatch()
+                        if (arguments.size != 2) return noMatch()
+                        val (num, type) = arguments
+                        MethodTypeHelper.changeParameterType(mhType, num, type)
+                    }
+
+                    "changeReturnType" -> {
+                        val mhType = qualifier?.mhType(block) ?: return noMatch()
+                        if (arguments.size != 1) return noMatch()
+                        val type = arguments[0]
+                        MethodTypeHelper.changeReturnType(mhType, type)
+                    }
+
+                    "appendParameterTypes" ->
+                        MethodTypeHelper.appendParameterTypes(qualifier?.mhType(block) ?: return noMatch(), arguments)
+
+                    "erase" ->
+                        MethodTypeHelper.erase(qualifier?.mhType(block) ?: return noMatch(), objectType(expression))
+
+                    "generic" ->
+                        MethodTypeHelper.generic(qualifier?.mhType(block) ?: return noMatch(), objectType(expression))
+
+                    "genericMethodType" -> {
+                        val size = arguments.size
+                        if (size != 1 && arguments.size != 2) return noMatch()
+                        val finalArray = if (size == 1) false else arguments[1].getConstantOfType<Boolean>() ?: return notConstant()
+                        MethodTypeHelper.genericMethodType(arguments[0], finalArray, objectType(expression))
+                    }
+
+                    "fromMethodDescriptorString" -> notConstant() // not supported
+
                     "describeConstable",
                     "descriptorString",
                     "hasPrimitives",
@@ -92,7 +148,8 @@ class SsaAnalyzer(private val controlFlow: ControlFlow, private val typeData: Ty
                     "parameterCount",
                     "parameterList",
                     "parameterType",
-                    "toMethodDescriptorString", -> noMethodHandle()
+                    "toMethodDescriptorString",
+                    -> noMethodHandle()
 
                     in objectMethods -> noMethodHandle()
                     else -> warnUnsupported(expression, "MethodType")
@@ -108,7 +165,7 @@ class SsaAnalyzer(private val controlFlow: ControlFlow, private val typeData: Ty
                     "asVarargsCollector" -> TODO()
                     "bindTo" -> {
                         if (arguments.size != 1) return noMatch()
-                        val target = expression.methodExpression.qualifierExpression ?: return noMatch()
+                        val target = qualifier ?: return noMatch()
                         val objectType = arguments[0].type ?: return notConstant()
                         MethodHandleTransformer.bindTo(target.mhTypeOrNoMatch(block), objectType)
                     }
@@ -163,7 +220,6 @@ class SsaAnalyzer(private val controlFlow: ControlFlow, private val typeData: Ty
             "constant" -> {
                 if (arguments.size != 2) return noMatch()
                 val type = arguments[0].getConstantOfType<PsiType>() ?: return notConstant()
-                // TODO warn if value is incompatible with type?
                 MethodHandlesInitializer.constant(type)
             }
 
--- src/main/kotlin/de/sirywell/methodhandleplugin/mhtype/MethodTypeHelper.kt
+++ src/main/kotlin/de/sirywell/methodhandleplugin/mhtype/MethodTypeHelper.kt
@@ -1,35 +1,129 @@
 package de.sirywell.methodhandleplugin.mhtype
 
-import de.sirywell.methodhandleplugin.MethodHandleSignature.Companion.create
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiExpression
+import com.intellij.psi.PsiPrimitiveType
 import com.intellij.psi.PsiType
+import de.sirywell.methodhandleplugin.MHS
+import de.sirywell.methodhandleplugin.MethodHandleSignature.Companion.create
+import de.sirywell.methodhandleplugin.getConstantOfType
+import java.util.Collections.nCopies
 
 object MethodTypeHelper {
 
-    fun appendParameterTypes(mhType: MhType, ptypesToInsert: List<PsiExpression>): MhType = TODO()
+    fun appendParameterTypes(mhType: MhType, ptypesToInsert: List<PsiExpression>): MhType {
+        if (mhType !is MhSingleType) return mhType
+        val psiTypes = ptypesToInsert.map { it.getConstantOfType<PsiType>() ?: return Top }
+        // TODO check types for void
+        val parameters = mhType.signature.parameters
+        if (psiTypes.isEmpty()) return mhType // no change
+        return withParameters(mhType, parameters + psiTypes)
+    }
 
-    fun changeParameterType(mhType: MhType, num: PsiExpression, nptype: PsiExpression): MhType = TODO()
+    fun changeParameterType(mhType: MhType, num: PsiExpression, nptype: PsiExpression): MhType {
+        if (mhType !is MhSingleType) return mhType
+        val type = nptype.getConstantOfType<PsiType>() ?: return Top // TODO inspection on void
+        val pos = num.getConstantOfType<Int>() ?: return Top
+        val parameters = mhType.signature.parameters
+        if (pos < 0 || pos > parameters.size) return Top // TODO inspection
+        val mutable = parameters.toMutableList()
+        mutable[pos] = type
+        return withParameters(mhType, mutable)
+    }
 
-    fun changeReturnType(mhType: MhType, num: PsiExpression, nptype: PsiExpression): MhType = TODO()
+    fun changeReturnType(mhType: MhType, nrtype: PsiExpression): MhType {
+        if (mhType !is MhSingleType) return mhType
+        val type = nrtype.getConstantOfType<PsiType>() ?: return Top // TODO inspection on void
+        return mhType.withSignature(mhType.signature.withReturnType(type))
+    }
 
-    fun dropParameterTypes(mhType: MhType, start: PsiExpression, end: PsiExpression): MhType = TODO()
+    fun dropParameterTypes(mhType: MhType, start: PsiExpression, end: PsiExpression): MhType {
+        if (mhType !is MhSingleType) return mhType
+        val startIndex = start.getConstantOfType<Int>() ?: return Bot
+        val endIndex = end.getConstantOfType<Int>() ?: return Bot
+        val parameters = mhType.signature.parameters
+        if (invalidRange(parameters.size, startIndex, endIndex)) return Top // TODO inspection
+        val mutable = parameters.toMutableList()
+        mutable.subList(startIndex, endIndex).clear()
+        return withParameters(mhType, mutable)
+    }
 
-    fun erase(mhType: MhType): MhType = TODO()
+    fun erase(mhType: MhType, objectType: PsiType): MhType {
+        if (mhType !is MhSingleType) return Bot
+        val params = mhType.signature.parameters.map { it.erase(objectType) }
+        val ret = mhType.signature.returnType.erase(objectType)
+        return mhType.withSignature(MHS.create(ret, params))
+    }
 
-    fun generic(mhType: MhType, objectArgCount: PsiExpression, finalArray: PsiExpression? = null): MhType = TODO()
+    fun generic(mhType: MhType, objectType: PsiType): MhType {
+        if (mhType !is MhSingleType) return mhType
+        val size = mhType.signature.parameters.size
+        return mhType.withSignature(MHS.create(objectType, nCopies(size, objectType)))
+    }
 
-    fun insertParameterTypes(num: PsiExpression, ptypesToInsert: List<PsiExpression>): MhType = TODO()
+    fun genericMethodType(objectArgCount: PsiExpression, finalArray: Boolean, objectType: PsiType): MhType {
+        val count = objectArgCount.getConstantOfType<Int>() ?: return Bot
+        var parameters = nCopies(count, objectType)
+        if (finalArray) {
+            parameters = parameters + objectType.createArrayType()
+        }
+        return MhExactType(MHS.create(objectType, parameters))
+    }
 
-    fun methodType(args: List<PsiType> = listOf()): MhType {
-        if (args.isEmpty()) return Bot
-        val rtype = args[0]
-        val params = args.drop(1)
+    fun insertParameterTypes(mhType: MhType, num: PsiExpression, ptypesToInsert: List<PsiExpression>): MhType {
+        if (mhType !is MhSingleType) return mhType
+        val psiTypes = ptypesToInsert.map { it.getConstantOfType<PsiType>() ?: return Top }
+        // TODO check types for void
+        val parameters = mhType.signature.parameters
+        val pos = num.getConstantOfType<Int>() ?: return Top
+        if (pos < 0 || pos > parameters.size) return Top // TODO inspection
+        if (psiTypes.isEmpty()) return mhType // no change
+        val mutable = parameters.toMutableList()
+        mutable.addAll(pos, psiTypes)
+        return withParameters(mhType, mutable)
+    }
+
+    private fun withParameters(
+        mhType: MhSingleType,
+        newParameters: List<PsiType>
+    ) = mhType.withSignature(mhType.signature.withParameters(newParameters))
+
+    private fun invalidRange(parametersSize: Int, start: Int, end: Int): Boolean {
+        return start < 0 || start > parametersSize || end < 0 || end > parametersSize || start > end
+    }
+
+    fun methodType(args: List<PsiExpression>): MhType {
+        val rtype = args[0].getConstantOfType<PsiType>() ?: return Bot
+        val params = args.drop(1).map { it.getConstantOfType<PsiType>() ?: return Bot }
         return MhExactType(create(rtype, params.toList()))
     }
 
-    fun unwrap(mhType: MhType): MhType = TODO()
+    fun methodType(rtype: PsiExpression, mhType: MhType): MhType {
+        if (mhType !is MhSingleType) return mhType
+        val type = rtype.getConstantOfType<PsiType>() ?: return Bot
+        return mhType.withSignature(mhType.signature.withReturnType(type))
+    }
 
-    fun wrap(mhType: MhType): MhType = TODO()
+    fun unwrap(mhType: MhType): MhType = mapTypes(mhType, this::unwrap)
 
-    private fun PsiType.erase(): PsiType = TODO()
+    fun wrap(context: PsiElement, mhType: MhType): MhType = mapTypes(mhType) { wrap(context, it) }
+
+    private fun mapTypes(mhType: MhType, map: (PsiType) -> PsiType): MhType {
+        if (mhType !is MhSingleType) return mhType
+        val ret = map(mhType.signature.returnType)
+        val params = mhType.signature.parameters.map { map(it) }
+        return mhType.withSignature(MHS.create(ret, params))
+    }
+
+    private fun PsiType.erase(objectType: PsiType): PsiType {
+        if (this is PsiPrimitiveType) return this
+        return objectType
+    }
+
+    private fun unwrap(type: PsiType): PsiType = PsiPrimitiveType.getOptionallyUnboxedType(type) ?: type
+
+    private fun wrap(context: PsiElement, type: PsiType): PsiType {
+        if (type !is PsiPrimitiveType) return type
+        return type.getBoxedType(context) ?: type
+    }
 }
