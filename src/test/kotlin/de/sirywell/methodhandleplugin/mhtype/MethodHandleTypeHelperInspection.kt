666b4e39827dc590608992f25a24a6425d32c152
SirYwell <hannesgreule@outlook.de>
2023-07-03 17:04:09 +0200

    introduce type-checking tests via custom inspection

---
+++ src/test/kotlin/de/sirywell/methodhandleplugin/mhtype/MethodHandleTypeHelperInspection.kt
@@ -0,0 +1,25 @@
+package de.sirywell.methodhandleplugin.mhtype
+
+import com.intellij.codeInspection.LocalInspectionTool
+import com.intellij.codeInspection.ProblemHighlightType
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiElementVisitor
+import de.sirywell.methodhandleplugin.TypeData
+
+/**
+ * We (mis)use this inspection to add the type info as highlighting to the source code.
+ * This way, we can reuse existing test infrastructure that is meant to assert presence of inspection results.
+ * I'm aware this is a bit hacky, but it's just too simple to go a different route.
+ */
+class MethodHandleTypeHelperInspection : LocalInspectionTool() {
+
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+        return object : PsiElementVisitor() {
+            override fun visitElement(element: PsiElement) {
+                val foundType = TypeData.forFile(element.containingFile)[element] ?: return
+                holder.registerProblem(element, foundType.toString(), ProblemHighlightType.INFORMATION)
+            }
+        }
+    }
+}
