9d9ae29f5df8776c50fd611cd45c96520e0be8d5
Hannes Greule <SirYwell@users.noreply.github.com>
2023-09-23 20:23:29 +0200

    test: add tests covering array-specific MethodHandle initializers (#39)

---
+++ src/test/testData/InitializeArrayElementSetter.java
@@ -0,0 +1,6 @@
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+
+class InitializeArraySetter {
+  <info descr="(double[],int,double)void">private static final MethodHandle MH = <info descr="(double[],int,double)void">MethodHandles.arrayElementSetter(double[].class)</info>;</info>
+}
---
+++ src/test/testData/InitializeArrayLength.java
@@ -0,0 +1,6 @@
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+
+class InitializeArrayLength {
+  <info descr="(double[])int">private static final MethodHandle MH = <info descr="(double[])int">MethodHandles.arrayLength(double[].class)</info>;</info>
+}
