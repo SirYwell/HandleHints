53e4e08781f515ea3661968e386d5f859f53664a
SirYwell <hannesgreule@outlook.de>
2024-06-22 08:32:28 +0200

    add negative tests

--- src/test/testData/LookupFindVarHandle.java
+++ src/test/testData/LookupFindVarHandle.java
@@ -7,7 +7,14 @@ class LookupFindVarHandle {
   int myField;
   static {
     try {
-      <info descr="(LookupFindVarHandle)(int)">VarHandle vh = <info descr="(LookupFindVarHandle)(int)">MethodHandles.lookup().findVarHandle(LookupFindVarHandle.class, "myField", int.class)</info>;</info>
+      // valid
+      <info descr="(LookupFindVarHandle)(int)">VarHandle vh0 = <info descr="(LookupFindVarHandle)(int)">MethodHandles.lookup().findVarHandle(LookupFindVarHandle.class, "myField", int.class)</info>;</info>
+      // invalid - field cannot be of type void
+      <info descr="(LookupFindVarHandle)(⊤)">VarHandle vh1 = <info descr="(LookupFindVarHandle)(⊤)">MethodHandles.lookup().findVarHandle(LookupFindVarHandle.class, "myField", void.class)</info>;</info>
+      // invalid - target class cannot be void
+      <info descr="(⊤)(int)">VarHandle vh2 = <info descr="(⊤)(int)">MethodHandles.lookup().findVarHandle(void.class, "myField", int.class)</info>;</info>
+      // invalid - target class cannot be int
+      <info descr="(⊤)(int)">VarHandle vh3 = <info descr="(⊤)(int)">MethodHandles.lookup().findVarHandle(int.class, "myField", int.class)</info>;</info>
     } catch (Throwable ignored) {}
   }
 }
