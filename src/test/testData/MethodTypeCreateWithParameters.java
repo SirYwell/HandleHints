16d7ca813d5db94bc1359121bd62af417b2c019e
Hannes Greule <SirYwell@users.noreply.github.com>
2024-10-18 23:11:21 +0200

    feature: warn on void MethodType parameters (#135)

--- src/test/testData/MethodTypeCreateWithParameters.java
+++ src/test/testData/MethodTypeCreateWithParameters.java
@@ -2,6 +2,9 @@ import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 
-class MethodTypeWrap {
+class MethodTypeCreateWithParameters {
   <info descr="(int,float)void">private static final MethodType A = <info descr="(int,float)void">MethodType.methodType(void.class, <info descr="(int,float)String">MethodType.methodType(String.class, int.class, float.class)</info>)</info>;</info>
+  void create() {
+    <info descr="(⊤)void">MethodType mt00 = <info descr="(⊤)void">MethodType.methodType(void.class, <warning descr="Type must not be void.">void.class</warning>)</info>;</info>
+  }
 }
