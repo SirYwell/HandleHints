f711a07de2186248eb787e22ee4b24fe9cdc0caa
SirYwell <hannesgreule@outlook.de>
2024-12-29 15:22:26 +0100

    feature: improve MethodHandles#dropArguments analysis

--- src/test/testData/MethodTypeChangeParameterType.java
+++ src/test/testData/MethodTypeChangeParameterType.java
@@ -6,6 +6,6 @@ class MethodTypeChangeParameterType {
   <info descr="(double,int)void">private static final MethodType A = <info descr="(double,int)void"><info descr="(boolean,int)void">MethodType.methodType(void.class, boolean.class, int.class)</info>.changeParameterType(0, double.class)</info>;</info>
   <info descr="(boolean,double)void">private static final MethodType B = <info descr="(boolean,double)void"><info descr="(boolean,int)void">MethodType.methodType(void.class, boolean.class, int.class)</info>.changeParameterType(1, double.class)</info>;</info>
   void change(MethodType unknown) {
-    <info descr="⊤">MethodType mt40 = <info descr="⊤">unknown.changeParameterType(0, <warning descr="Type must not be void.">void.class</warning>)</info>;</info>
+    <info descr="(0=⊤)⊤">MethodType mt40 = <info descr="(0=⊤)⊤">unknown.changeParameterType(0, <warning descr="Type must not be void.">void.class</warning>)</info>;</info>
   }
 }
